5cad56de9be7d33b280801b4f3077b71901dd023
Owen <owenrham@gmail.com>
2015-03-06 16:08:26 -0500

    Add geocode

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,11 +10,13 @@ class Tweet
   end
 
   def tweets(search)
-    @client.search("#{search} -rt", lang: "en").take(10)
-      .map{|t| [t.user.screen_name, t.text, analyze(t.text)]}
+    @client.search("#{search} -rt", geocode: "38.9047,-77.0164,100mi", lang: "en").take(100).collect
   end
 
   def analyze(text)
     HTTParty.post("http://sentiment.vivekn.com/api/text/", body: { txt: text } )
   end
 end
+# .map{|t| t}
+
+  # [t.user.screen_name, t.text, analyze(t.text)]}
