4d629d67241a59fcb18a617b591e9f9f5dbef61f
Owen <owenrham@gmail.com>
2015-03-06 12:08:05 -0500

    Remove initialize file.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,7 +10,7 @@ class Tweet
   end
 
   def tweets(search)
-    @client.search("##{search} -rt", lang: "en").take(10)
+    @client.search("#{search} -rt", lang: "en").take(10)
       .map{|t| [t.user.screen_name, t.text, analyze(t.text)]}
   end
 
@@ -18,29 +18,3 @@ class Tweet
     HTTParty.post("http://sentiment.vivekn.com/api/text/", body: { txt: text } )
   end
 end
-
-
-
-#   def initialize(search)
-#     @content = HTTParty.get(
-#     "https://api.twitter.com/1.1/search/tweets.json?q=#{search}",
-#     :headers => {"Authorization" => "token #{ENV['TWITTER_KEY']}",
-#     "User-Agent" => "anyone"
-#   }
-#   )
-# end
-# class ProfileController < ApplicationController
-#   def show
-#     profile_url = "https://api.github.com/users/Fallonious/repos"
-#     @profile = HTTParty.get(profile_url, login_info)
-#
-#     avatar_url = "https://api.github.com/users/Fallonious"
-#     @avatar = HTTParty.get(avatar_url, login_info) ["avatar_url"]
-#   end
-#
-#   private def login_info
-#     {:headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
-#     "User-Agent" => "User-Agent"}
-#   }
-# end
-# end
